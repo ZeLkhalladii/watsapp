25af27f151d1dbe69f3b59c54b8fb5ae219b7f21
zineb elkhallai <zinebelkhalladi.2019@gmail.com>
2020-03-03 17:20:22 +0100

    chat

---
+++ wtsp.js
@@ -0,0 +1,20 @@
+function send1() {
+   var input = document.getElementById("input1");
+
+       var div1 = document.getElementById("msg1");
+       var text = document.createElement("p");
+       var textnode = document.createTextNode(input.value);
+      text.appendChild(textnode);
+      div1.appendChild(text);
+}
+
+function send2() {
+    var input = document.getElementById("input1");
+ 
+        var div1 = document.getElementById("msg1");
+        var text = document.createElement("p");
+        text.setAttribute('class','text2');
+        var textnode = document.createTextNode(input.value);
+       text.appendChild(textnode);
+       div1.appendChild(text);
+ }
