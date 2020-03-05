a5030f108a1a453e8809b6b2fd005392d7b2c364
zineb elkhallai <zinebelkhalladi.2019@gmail.com>
2020-03-05 11:53:52 +0100

    whathsapp

--- wtsp.js
+++ wtsp.js
@@ -3,6 +3,7 @@ function send1() {
 
        var div1 = document.getElementById("msg1");
        var text = document.createElement("p");
+       text.setAttribute('class','text1');
        var textnode = document.createTextNode(input.value);
       text.appendChild(textnode);
       div1.appendChild(text);
